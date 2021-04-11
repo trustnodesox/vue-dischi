db999cf7314cd4324e513d0c9bd0b1cfd2dd238c
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-04-12 00:37:47 +0200

    completo senza bonus

---
+++ main.js
@@ -0,0 +1,24 @@
+Vue.config.devtools = true;
+
+var app = new Vue({
+  el: '#root',
+  data: {
+    dischi: [],
+
+  },
+  mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then((response) => {
+      for (var i = 0; i < response.data.response.length; i++) {
+        this.dischi.push(response.data.response[i])
+      }
+      for (var i = 0; i < this.dischi.length; i++) {
+        console.log(this.dischi[i].poster)
+      };
+
+      console.log(response.data.response)
+
+
+    });
+  }
+})
